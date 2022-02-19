c61a69cd03dbfcda07ed1683251783bee00a103b
les1er1 <93589226+les1er1@users.noreply.github.com>
2022-02-19 20:08:55 +0300

    Add files via upload

---
+++ multi_mapping.lua
@@ -0,0 +1,132 @@
+local sw, sh = getScreenResolution()
+require "lib.moonloader"
+local imgui = require 'mimgui'
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+u8 = encoding.UTF8
+local sampev = require 'lib.samp.events'
+
+font = renderCreateFont('Arial', 8, 5)
+
+local mp = {
+    airbreak = false,
+    airbreakSpeed = 1.0,
+    ------------
+    lastObjectClone = 0,
+    lastObject = 0,
+    ------------
+    objRender = false
+}
+
+local backgroundDraw = imgui.OnFrame(
+    function() return true end,
+    function(self)
+        self.HideCursor = true
+        local dl = imgui.GetBackgroundDrawList()
+        local p = imgui.GetCursorScreenPos()
+        renderDrawBoxWithBorder(-2, sh - 30, sw + 3, 30, 0x8F000000, 2, 0xFF000000)
+        dl:AddText(imgui.ImVec2(13, sh - 22), 0xFFFFFFFF, 'MULTI-MAPPING Build #0001');
+        local x, y, z = getCharCoordinates(PLAYER_PED)
+        local angle = math.ceil(getCharHeading(PLAYER_PED))
+        if mp.airbreak then dl:AddText(imgui.ImVec2(330, sh - 22), 0xFF32CD32, '[ AirBreak ]') else dl:AddText(imgui.ImVec2(330, sh - 22), 0xFFC0C0C0, '[ AirBreak ]') end
+        if mp.objRender then dl:AddText(imgui.ImVec2(573, sh - 22), 0xFF32CD32, '[ Object ID Render ]') else dl:AddText(imgui.ImVec2(573, sh - 22), 0xFFC0C0C0, '[ Object ID Render ]') end
+        if mp.lastObjectClone == 0 then dl:AddText(imgui.ImVec2(400, sh - 22), 0xFFC0C0C0, '[       Last Object Clone        ]') elseif mp.lastObjectClone == 1 then dl:AddText(imgui.ImVec2(400, sh - 22), 0xFF32CD32, '[ Last Object Clone (MOUSE) ]') elseif mp.lastObjectClone == 2 then dl:AddText(imgui.ImVec2(400, sh - 22), 0xFF32CD32, '[ Last Object Clone (    ID    ) ]') end
+        dl:AddText(imgui.ImVec2(180, sh - 22), 0xFFFFFF66, math.round(x, 1)..' '..math.round(y, 1)..' '..math.round(z, 1)..' '..angle)
+        if isKeyDown(VK_F9) then setClipboardText(math.round(x, 1)..' '..math.round(y, 1)..' '..math.round(z, 1)) end
+        if isKeyDown(VK_F10) then setClipboardText(math.round(x, 1)..' '..math.round(y, 1)..' '..math.round(z, 1)..' '..angle) end
+        if not isSampfuncsConsoleActive() and not sampIsChatInputActive() and not isPauseMenuActive() then
+            if isKeyJustPressed(VK_Z) then
+                if mp.lastObjectClone == 2 then
+                    mp.lastObjectClone = 0
+                    return end
+                mp.lastObjectClone = mp.lastObjectClone + 1
+             end
+             if isKeyJustPressed(VK_O) then
+                mp.objRender = not mp.objRender
+             end
+             if isKeyJustPressed(VK_B) then sampSendChat('/oedit') end
+        end
+    end
+)
+
+function main()
+    while not isSampAvailable() do wait(0) end
+    imgui.Process = true
+    while true do wait(0)
+		if isKeyJustPressed(VK_RSHIFT) then
+            if not isSampfuncsConsoleActive() and not sampIsChatInputActive() and not isPauseMenuActive() then
+                mp.airbreak = not mp.airbreak
+                if mp.airbreak then
+                    local posX, posY, posZ = getCharCoordinates(playerPed)
+                    airBrkCoords = {posX, posY, posZ, 0.0, 0.0, getCharHeading(playerPed)}
+                end
+            end
+		end
+        if isKeyJustPressed(VK_X) then
+            if not isSampfuncsConsoleActive() and not sampIsChatInputActive() and not isPauseMenuActive() then
+                if mp.lastObjectClone == 1 then sampSendChat('/clone') end
+                if mp.lastObjectClone == 2 then sampSendChat('/clone '..mp.lastObject) end
+            end
+        end
+        if mp.airbreak == true then
+            al()
+        end
+        if mp.objRender then
+            for _, v in pairs(getAllObjects()) do
+                if isObjectOnScreen(v) then
+                    local _, x, y, z = getObjectCoordinates(v)
+                    local x1, y1 = convert3DCoordsToScreen(x,y,z)
+                    local model = getObjectModel(v)
+                    renderFontDrawText(font, model, x1, y1, -1)
+                end
+            end
+        end
+    end
+end
+
+function al()
+    if mp.airbreak then -- airbrake
+        if isCharInAnyCar(playerPed) then heading = getCarHeading(storeCarCharIsInNoSave(playerPed))
+        else heading = getCharHeading(playerPed) end
+        local camCoordX, camCoordY, camCoordZ = getActiveCameraCoordinates()
+        local targetCamX, targetCamY, targetCamZ = getActiveCameraPointAt()
+        local angle = getHeadingFromVector2d(targetCamX - camCoordX, targetCamY - camCoordY)
+        if isCharInAnyCar(playerPed) then difference = 0.79 else difference = 1.0 end
+        setCharCoordinates(playerPed, airBrkCoords[1], airBrkCoords[2], airBrkCoords[3] - difference)
+        if not isSampfuncsConsoleActive() and not sampIsChatInputActive() and not isPauseMenuActive() then
+            if isKeyDown(VK_W) then
+                airBrkCoords[1] = airBrkCoords[1] + mp.airbreakSpeed * math.sin(-math.rad(angle))
+                airBrkCoords[2] = airBrkCoords[2] + mp.airbreakSpeed * math.cos(-math.rad(angle))
+                if not isCharInAnyCar(playerPed) then setCharHeading(playerPed, angle)
+                else setCarHeading(storeCarCharIsInNoSave(playerPed), angle) end
+            elseif isKeyDown(VK_S) then
+                airBrkCoords[1] = airBrkCoords[1] - mp.airbreakSpeed * math.sin(-math.rad(heading))
+                airBrkCoords[2] = airBrkCoords[2] - mp.airbreakSpeed * math.cos(-math.rad(heading))
+            end
+            if isKeyDown(VK_A) then
+                airBrkCoords[1] = airBrkCoords[1] - mp.airbreakSpeed * math.sin(-math.rad(heading - 90))
+                airBrkCoords[2] = airBrkCoords[2] - mp.airbreakSpeed * math.cos(-math.rad(heading - 90))
+            elseif isKeyDown(VK_D) then
+                airBrkCoords[1] = airBrkCoords[1] - mp.airbreakSpeed * math.sin(-math.rad(heading + 90))
+                airBrkCoords[2] = airBrkCoords[2] - mp.airbreakSpeed * math.cos(-math.rad(heading + 90))
+            end
+            if isKeyDown(VK_SPACE) or isKeyDown(VK_UP) then airBrkCoords[3] = airBrkCoords[3] + mp.airbreakSpeed / 2.0 end
+            if isKeyDown(VK_LSHIFT) or isKeyDown(VK_DOWN) and airBrkCoords[3] > -95.0 then airBrkCoords[3] = airBrkCoords[3] - mp.airbreakSpeed / 2.0 end
+            if isKeyDown(VK_ADD) then if mp.airbreakSpeed < 3.0 then mp.airbreakSpeed = mp.airbreakSpeed + 0.1 else mp.airbreakSpeed = 3.0 end end
+            if isKeyDown(VK_SUBTRACT) then if mp.airbreakSpeed > 0.1 then mp.airbreakSpeed = mp.airbreakSpeed - 0.1 else mp.airbreakSpeed = 0.1 end end
+        end
+    end
+end
+
+function math.round(num, idp)
+    local mult = 10^(idp or 0)
+    return math.floor(num * mult + 0.5) / mult
+end
+
+function sampev.onServerMessage(color, text)
+    if mp.lastObjectClone == 2 then
+        if text:find('Создан объект: (%d+)') then
+            mp.lastObject = text:match('Создан объект: (%d+)')
+        end
+    end
+end
